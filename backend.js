924fd93893896e63ae34e75c57eb22fd76bb3242
shijinj4 <67093391+shijinj4@users.noreply.github.com>
2024-03-17 16:26:29 -0400

    Update backend.js

--- backend.js
+++ backend.js
@@ -2,13 +2,9 @@
 
 const express = require('express');
 const app = express();
-const cors = require('cors');
-
-// Middleware for handling CORS (Cross-Origin Resource Sharing)
-app.use(cors());
 
 // Endpoint to serve data
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
     // Your code to fetch data from the database or any other source
     // For this example, let's just send back dummy data
     const data = { message: 'Hello from backend!' };
